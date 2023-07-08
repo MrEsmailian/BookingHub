9bba50a4223869b3c0dee733dc9493e2ab746ac7
Mohammad-Reza <117102152+ConstantineMR@users.noreply.github.com>
2023-07-08 19:08:23 +0330

    toast

--- JS/script-sl.js
+++ JS/script-sl.js
@@ -1,5 +1,3 @@
-document.getElementById("log").addEventListener("submit", logIn());
-document.getElementById("sign").addEventListener("submit", signUp());
 document.getElementById("SPassword").addEventListener('input', passStrength());
 function logIn(){
     let username = document.getElementById("LUsername").value;
@@ -17,23 +15,38 @@ function logIn(){
 }
 function signUp(){
     let username = document.getElementById("SUsername").value;
-    let phone = document.getElementById("SPhone").value;
+    let first_name = "Mammad";
+    let last_name = "kachal";
+    let address = "mashhad";
+    let phone_number = document.getElementById("SPhone").value;
     let email = document.getElementById("SEmail").value;
     let password = document.getElementById("SPassword").value;
     let confirm = document.getElementById("SConfirm").value;
-
-    if (username.value === "" || phone.value === "" || email.value === "" || password.value === "") {
+    if (username.value === "" || phone_number.value === "" || email.value === "" || password.value === "") {
         alert("Ensure you input a value in all fields!");
     } else if (password.value !== confirm.value) {
         alert("Ensure you have entered same password and confirmation")
     } else {
         var data = {
             username,
-            phone,
+            first_name,
+            last_name,
+            address,
+            phone_number,
             email,
             password
         };
         var entry = JSON.stringify(data);
+        sign(username, first_name, last_name, address, phone_number, email, password);
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('POST', 'http://127.0.0.1:8000/api/account/', true);
+        // xhr.setRequestHeader('Content-Type', 'application/json');
+        // xhr.onreadystatechange = function (){
+        //     if (xhr.readyState === 4 && xhr.status === 201) {
+        //         alert(JSON.parse(xhr.responseText));
+        //     }
+        // }
+        // xhr.send(entry);
     }
 }
 function passStrength(){
@@ -52,4 +65,38 @@ function passStrength(){
             strength.style.width = value + "%";
         }
     });
+}
+
+function sign(username, first_name, last_name, address, phone_number, email, password) {
+    return new Promise((resolve, reject) => {
+        try {
+            $.ajax({
+                url: 'http:localhost:8000'/api/account,
+                type: 'POST',
+                data: JSON.stringify({ username, first_name, last_name, address, phone_number, email, password }),
+                contentType: 'application/json',
+            }).done((response) => {
+                if (response['authenticated']){
+                    resolve({state: 'done', content: response});
+                    alert(2);
+                }else{
+                    resolve({state: 'error', content: 'Email or password is incorrect'});
+                    alert(3);
+                }
+            }).fail((errorThrown) => {
+                reject({state: 'fail', content: errorThrown});
+                alert(4);
+            });
+        }catch(error) {
+            reject({state: 'catch', content: error});
+            alert(5);
+        }
+    });
+}
+
+function toastFunction(msg) {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = msg;
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
